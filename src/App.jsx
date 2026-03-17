807d9909a9a413ce839ba409296a2b8a728a3775
luisangel2222 <74742371+luisangel566@users.noreply.github.com>
2026-03-17 09:35:15 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,37 @@
-import { Routes, Route } from 'react-router-dom' // [EXISTENTE]
-import Navegacion from './componentes/Navegacion' // [EXISTENTE]
-import ListadoLibros from './libros/ListadoLibros' // [EXISTENTE]
-import AgregarLibro from './libros/AgregarLibro' // [EXISTENTE]
-import EditarLibro from './libros/EditarLibro' // [NUEVO]
+import { Routes, Route, Navigate } from 'react-router-dom'
+import Navegacion from './componentes/Navegacion'
+import ListadoLibros from './libros/ListadoLibros'
+import AgregarLibro from './libros/AgregarLibro'
+import EditarLibro from './libros/EditarLibro'
 
-function App() {
+// Layout reutilizable
+function Layout({ children }) {
   return (
     <>
       <Navegacion />
       <div className="container mt-4">
-        <Routes>
-          <Route path="/" element={<ListadoLibros />} />
-          <Route path="/agregar" element={<AgregarLibro />} />
-          <Route path="/editar/:id" element={<EditarLibro />} /> {/* [NUEVO] */}
-        </Routes>
+        {children}
       </div>
     </>
+  )
+}
+
+function App() {
+  return (
+    <Layout>
+      <Routes>
+        {/* Rutas principales */}
+        <Route path="/" element={<ListadoLibros />} />
+        <Route path="/agregar" element={<AgregarLibro />} />
+        <Route path="/editar/:id" element={<EditarLibro />} />
+
+        {/* Redirección opcional */}
+        <Route path="/home" element={<Navigate to="/" />} />
+
+        {/* Ruta 404 */}
+        <Route path="*" element={<h2 className="text-center">404 - Página no encontrada</h2>} />
+      </Routes>
+    </Layout>
   )
 }
 
