490cbcee2d2049493656c3d42139dc6d93f1f53e
luisangel2222 <74742371+luisangel566@users.noreply.github.com>
2026-03-17 09:35:54 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom' // [NUEVO]
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+// Validación del root (evita errores silenciosos)
+const rootElement = document.getElementById('root')
+
+if (!rootElement) {
+  throw new Error('No se encontró el elemento root en el DOM')
+}
+
+const root = createRoot(rootElement)
+
+root.render(
   <StrictMode>
-    <BrowserRouter> {/* [NUEVO] */}
+    <BrowserRouter>
       <App />
-    </BrowserRouter> {/* [NUEVO] */}
-  </StrictMode>,
+    </BrowserRouter>
+  </StrictMode>
 )
